2b3787cf17dd642ddb2c439616c9b3aa0b549ce5
Prashant <prashantsherkar7515400@gmail.com>
2024-12-10 17:18:37 +0530

    initial commit

---
+++ app/q1/page.tsx
@@ -0,0 +1,11 @@
+export default function q1() {
+    const arr = [1, 2, 3, 4, 5];
+    const result = arr.map((item, index) => item * index);
+    
+    return (
+        <div>
+            <h1>Q1</h1>
+        </div>
+    )
+    
+}
