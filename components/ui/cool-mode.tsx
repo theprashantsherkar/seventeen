e24653ec072f8efeac76d582965b5e6b09614ab1
Prashant <prashantsherkar7515400@gmail.com>
2024-12-14 12:08:25 +0530

    ssbkf

--- components/ui/cool-mode.tsx
+++ components/ui/cool-mode.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { ReactNode, RefObject, useEffect, useRef } from "react";
+import React, { ReactNode, useEffect, useRef } from "react";
 
 export interface BaseParticle {
   element: HTMLElement | SVGSVGElement;
@@ -29,7 +29,7 @@ export interface CoolParticleOptions extends BaseParticleOptions {
 
 const getContainer = () => {
   const id = "_coolMode_effect";
-  let existingContainer = document.getElementById(id);
+  const existingContainer = document.getElementById(id);
 
   if (existingContainer) {
     return existingContainer;
